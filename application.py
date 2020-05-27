e6b5b06bf5680befdaf9bd50fef92d2e51e2e903
FergusFitzpatrick <fergusfitz@gmail.com>
2020-05-27 22:32:51 +0100

    Can select from database and display data in search.html. api/'isbn' still giving Internal Server Error.

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-DATABASE_URL = "postgres://joqityswncejjo:ce560d2222c9edcc16f30e8f95c840a7d8348c8bef7ca3b351ecdb32c63e3f48@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d7s7d50npqadm9"
+DATABASE_URL = """postgres://zymwknwojzdrxt:6b4a824b4c7e4f0f3e7db6f0939d4d75e206845a0ce6d1c7d87621e35635a226@ec2-35-173-94-156.compute-1.amazonaws.com:5432/d3qnplhrdgtkp0"""
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -27,3 +27,17 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     title = "Home"
     return render_template("index.html", title=title)
+
+@app.route("/search")
+def search():
+    books = db.execute("SELECT * FROM books").fetchall()
+    title = "Search"
+    return render_template("search.html", title=title, books=books)
+
+
+@app.route("/api/<int:isbn>")
+def api(isbn):
+    api = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    if book is None:
+        return render_template("error.html", message="No such book.")
+    return render_template("api.html", api=api)
